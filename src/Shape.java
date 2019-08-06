94a189b52f4ad38137a23ff80425a31c65681a9d
NoySegal <segalmaterial@gmail.com>
2019-08-06 14:21:03 +0300

    add vertices

--- src/Shape.java
+++ src/Shape.java
@@ -3,9 +3,11 @@ public abstract class Shape {
     private int ID;
     private static int counter = 0;
     private String color;
+    private Point[] vertices;
 
-    public Shape(String color) {
+    public Shape(String color, Point[] vertices) {
         this.color = color;
+        this.vertices = vertices;
         this.ID = counter;
         counter++;
     }
@@ -14,7 +16,11 @@ public abstract class Shape {
         return this.color;
     }
 
+    public void setColor(String newColor) { this.color = newColor; }
+
     public int getID() {
         return this.ID;
     }
+
+    public Point[] getVertices() { return this.vertices; }
 }
