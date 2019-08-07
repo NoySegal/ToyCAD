de57e586ada93f5eeebc3d9644b581580897774d
NoySegal <segalmaterial@gmail.com>
2019-08-07 17:43:46 +0300

    add deep-copy method

--- src/Point.java
+++ src/Point.java
@@ -9,4 +9,14 @@ public class Point extends Point2D.Double{
     public Point(Point point) {
         super(point.getX(), point.getY());
     }
+
+    public static Point[] copyPoints(Point[] originalPoints) {
+        Point[] copiedPoints = new Point[originalPoints.length];
+
+        for (int i = 0; i < originalPoints.length; i++) {
+            copiedPoints[i] = new Point(originalPoints[i]);
+        }
+
+        return copiedPoints;
+    }
 }
